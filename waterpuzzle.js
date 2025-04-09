58ddf829553f9e05a61b803b90cad94da45cf984
5b1g0006 <5b1g0006@stust.edu.tw>
2025-04-09 13:18:03 +0800

    新增試管生成和填充功能，更新相關事件處理

--- waterpuzzle.js
+++ waterpuzzle.js
@@ -16,9 +16,30 @@ document.addEventListener("DOMContentLoaded",()=>{
         chossLevel(selectedLevel);
     });
 
+    function createTubes(){
+        //gameContainer.innerHTML = "產生試管";
+        gameContainer.innerHTML = "";
+        tubes.length = 0; //清空試管
+
+        for(let i=0; i < levelCount + 1; i++){
+            const tube = document.createElement("div");
+            tube.classList.add("tube");
+            //要新增tube的事件處理樣式
+            gameContainer.appendChild(tube);
+            tubes.push(tube);
+        }
+    }
+
+    function fillTubes(){
+        gameContainer.innerHTML = "填滿試管顏色";
+    }
+
     playButton.addEventListener("click",()=>{
         //實作開始玩遊戲
         alert("開始玩遊戲");
+        tubes.length = 0; //清空試管
+        createTubes(); //產生試管
+        fillTubes(); //填滿試管顏色
 
     });
 });
